565b34597520aa3d6c0d8e77060528dbf1d1f92a
sammypotter <50476978+0spotter0@users.noreply.github.com>
2026-06-05 12:21:50 -0400

    move treesitter to main branch, add gj/gk remap for wrapped buffers, add autowrap rules for md/txt

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -25,6 +25,29 @@ autocmd({"BufWritePre"}, {
     command = [[%s/\s\+$//e]],
 })
 
+-- Map j/k to gj/gk in any buffer where 'wrap' is enabled
+local function sync_wrap_motions()
+    if vim.wo.wrap then
+        vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { buffer = true, expr = true })
+        vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { buffer = true, expr = true })
+    else
+        pcall(vim.keymap.del, 'n', 'j', { buffer = true })
+        pcall(vim.keymap.del, 'n', 'k', { buffer = true })
+    end
+end
+
+autocmd('OptionSet', {
+    group = custom_augroup,
+    pattern = "wrap",
+    callback = sync_wrap_motions,
+})
+
+autocmd({"BufWinEnter"}, {
+    group = custom_augroup,
+    pattern = "*",
+    callback = sync_wrap_motions,
+})
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,44 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   lazy = false,
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "c",
-        "cpp",
-        "rust",
-        "python",
-        "html",
-        "css",
-        "java",
-        "javascript",
-        "typescript",
-        "tsx",
-        "elixir",
-        "heex",
-        "json",
-        "yaml",
-        "bash",
-        "markdown",
-        "dockerfile",
-        "graphql",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+  config = function()
+    require("nvim-treesitter").setup()
+    require("nvim-treesitter").install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "c",
+      "cpp",
+      "rust",
+      "python",
+      "html",
+      "css",
+      "java",
+      "javascript",
+      "typescript",
+      "tsx",
+      "elixir",
+      "heex",
+      "json",
+      "yaml",
+      "bash",
+      "markdown",
+      "markdown_inline",
+      "dockerfile",
+      "graphql",
     })
-  end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("treesitter_start", { clear = true }),
+      callback = function(args)
+        local buf = args.buf
+        local ft = vim.bo[buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        pcall(vim.treesitter.start, buf, lang)
+      end,
+    })
+  end,
 }
